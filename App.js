b9df3f8bdd02d16a8a2cb4abac72a1d85de4f463
Syd Halperin <88507577+PG-shalperin@users.noreply.github.com>
2023-10-30 07:27:00 -0400

    Initial Commit

---
+++ App.js
@@ -0,0 +1,264 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, Button, Platform } from 'react-native';
+import * as SQLite from 'expo-sqlite';
+import { useState, useEffect } from 'react';
+import * as Sharing from 'expo-sharing';
+import * as FileSystem from 'expo-file-system';
+import * as DocumentPicker from 'expo-document-picker';
+
+// expo add expo-sqlite
+// expo add expo-file-system
+// expo add expo-document-picker
+// expo add expo-sharing
+// expo add expo-dev-client
+
+/*
+  For testing expo-document-picker on iOS we need a standalone app 
+  which is why we install expo-dev-client
+  
+  If you don't have eas installed then install using the following command:
+  npm install -g eas-cli
+
+  eas login
+  eas build:configure
+
+  Build for local development on iOS or Android:
+  eas build -p ios --profile development --local
+  OR
+  eas build -p android --profile development --local
+
+  May need to install the following to build locally (which allows debugging)
+  npm install -g yarn
+  brew install fastlane
+
+  After building install on your device:
+  For iOS (simulator): https://docs.expo.dev/build-reference/simulators/
+  For Android: https://docs.expo.dev/build-reference/apk/
+
+  Run on installed app:
+  expo start --dev-client
+*/
+
+
+export default function App() {
+  const [db, setDb] = useState(SQLite.openDatabase('example.db'));
+  const [isLoading, setIsLoading] = useState(true);
+  const [names, setNames] = useState([]);
+  const [currentName, setCurrentName] = useState(undefined);
+
+  // Function to insert a single name into the SQLite database
+    const insertNameToDb = (name) => {
+    return new Promise((resolve, reject) => {
+      db.transaction(tx => {
+        tx.executeSql('INSERT INTO names (name) values (?)', [name],
+          (txObj, resultSet) => {
+            resolve(resultSet.insertId);
+          },
+          (txObj, error) => {
+            console.log(error);
+            reject(error);
+          }
+        );
+      });
+    });
+  };
+
+  // Function to fetch names from the API
+  const fetchNamesFromAPI = async () => {
+    try {
+      const response = await fetch('https://example-data.draftbit.com/people?_limit=10');
+      const data = await response.json();
+      
+      // Upsert each full_name into the SQLite database using existing logic
+      const upsertPromises = data.map(person => {
+        return new Promise((resolve, reject) => {
+          db.transaction(tx => {
+            tx.executeSql('INSERT OR REPLACE INTO names (id, name) values (?, ?)', [person.id, person.full_name], // <-- Updated this line
+              (txObj, resultSet) => {
+                resolve(resultSet.insertId);
+              },
+              (txObj, error) => {
+                console.log(error);
+                reject(error);
+              }
+            );
+          });
+        });
+      });
+      await Promise.all(upsertPromises);
+
+      // Fetch names from SQLite to update the state
+      db.transaction(tx => {
+        tx.executeSql('SELECT * FROM names', null,
+          (txObj, resultSet) => setNames(resultSet.rows._array),
+          (txObj, error) => console.log(error)
+        );
+      });
+
+    } catch (error) {
+      console.error("Error fetching or saving names:", error);
+    }
+  };
+
+////////////////////////////////
+
+  const exportDb = async () => {
+    if (Platform.OS === "android") {
+      const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
+      if (permissions.granted) {
+        const base64 = await FileSystem.readAsStringAsync(
+          FileSystem.documentDirectory + 'SQLite/example.db',
+          {
+            encoding: FileSystem.EncodingType.Base64
+          }
+        );
+
+        await FileSystem.StorageAccessFramework.createFileAsync(permissions.directoryUri, 'example.db', 'application/octet-stream')
+        .then(async (uri) => {
+          await FileSystem.writeAsStringAsync(uri, base64, { encoding : FileSystem.EncodingType.Base64 });
+        })
+        .catch((e) => console.log(e));
+      } else {
+        console.log("Permission not granted");
+      }
+    } else {
+      await Sharing.shareAsync(FileSystem.documentDirectory + 'SQLite/example.db');
+    }
+  }
+
+  const importDb = async () => {
+    let result = await DocumentPicker.getDocumentAsync({
+      copyToCacheDirectory: true
+    });
+
+    if (result.type === 'success') {
+      setIsLoading(true);
+      
+      if (!(await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'SQLite')).exists) {
+        await FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'SQLite');
+      }
+
+      const base64 = await FileSystem.readAsStringAsync(
+        result.uri,
+        {
+          encoding: FileSystem.EncodingType.Base64
+        }
+      );
+
+      await FileSystem.writeAsStringAsync(FileSystem.documentDirectory + 'SQLite/example.db', base64, { encoding: FileSystem.EncodingType.Base64 });
+      await db.closeAsync();
+      setDb(SQLite.openDatabase('example.db'));
+    }
+  };
+
+  useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql('CREATE TABLE IF NOT EXISTS names (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)')
+    });
+
+    db.transaction(tx => {
+      tx.executeSql('SELECT * FROM names', null,
+        (txObj, resultSet) => setNames(resultSet.rows._array),
+        (txObj, error) => console.log(error)
+      );
+    });
+
+    // Fetch names from API and save them to the SQLite database
+    fetchNamesFromAPI();
+
+    setIsLoading(false);
+  }, [db]);
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading names...</Text>
+      </View>
+    );
+  }
+
+  const addName = () => {
+    db.transaction(tx => {
+      tx.executeSql('INSERT INTO names (name) values (?)', [currentName],
+        (txObj, resultSet) => {
+          let existingNames = [...names];
+          existingNames.push({ id: resultSet.insertId, name: currentName});
+          setNames(existingNames);
+          setCurrentName(undefined);
+        },
+        (txObj, error) => console.log(error)
+      );
+    });
+  }
+
+  const deleteName = (id) => {
+    db.transaction(tx => {
+      tx.executeSql('DELETE FROM names WHERE id = ?', [id],
+        (txObj, resultSet) => {
+          if (resultSet.rowsAffected > 0) {
+            let existingNames = [...names].filter(name => name.id !== id);
+            setNames(existingNames);
+          }
+        },
+        (txObj, error) => console.log(error)
+      );
+    });
+  };
+
+  const updateName = (id) => {
+    db.transaction(tx => {
+      tx.executeSql('UPDATE names SET name = ? WHERE id = ?', [currentName, id],
+        (txObj, resultSet) => {
+          if (resultSet.rowsAffected > 0) {
+            let existingNames = [...names];
+            const indexToUpdate = existingNames.findIndex(name => name.id === id);
+            existingNames[indexToUpdate].name = currentName;
+            setNames(existingNames);
+            setCurrentName(undefined);
+          }
+        },
+        (txObj, error) => console.log(error)
+      );
+    });
+  };
+
+  const showNames = () => {
+    return names.map((name, index) => {
+      return (
+        <View key={index} style={styles.row}>
+          <Text>{name.name}</Text>
+          <Button title='Delete' onPress={() => deleteName(name.id)} />
+          <Button title='Update' onPress={() => updateName(name.id)} />
+        </View>
+      );
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput value={currentName} placeholder='name' onChangeText={setCurrentName} />
+      <Button title="Add Name" onPress={addName} />
+      {showNames()}
+      <Button title="Export Db" onPress={exportDb} />
+      <Button title="Import Db" onPress={importDb} />
+      <Button title="Retrieve Names from API" onPress={fetchNamesFromAPI} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    justifyContent: 'space-between',
+    margin: 8
+  }
+});
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # rn-sqlite-api
+
+The purpose of this repo is to demo the use of React Native to query an API and then load the data from that api into a local sqlite database that can work in the event that no network is available. 
+
+* Consider forbidding the user from deleting the database rows when the network is unavailable.
+* Need to review watermelonDB to allow the user to resync the database once the network is available. 
